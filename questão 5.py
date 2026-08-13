6b204e1f71aaeba93729fcfad94e0fc4efef7cf8
Matheus <matheussilvabrito07@gmail.com>
2026-08-12 21:05:31 -0300

    Adiciona questão 5

---
+++ questão 5.py
@@ -0,0 +1,12 @@
+situacoes = [
+    "Presente",
+    "Ausente",
+    "Atrasado",
+    "Falta Justificada",
+    "Saída Antecipada",
+    "Licença Médica",
+    "Dispensado",
+    "Atividade Externa",
+    "Feriado",
+    "Suspenso"
+]
