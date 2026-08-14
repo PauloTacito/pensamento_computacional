47b0d19e6eb2144b90081cfd2714b1d80539a593
Marcos Paulo Soares bezerra <pauloglauciobezerra@gmail.com>
2026-08-14 10:12:52 -0300

    Add files via upload

---
+++ turmas.py
@@ -0,0 +1,53 @@
+turmas = [
+    {
+        "codigo_turma": "T01",
+        "disciplina": "Programacao",
+        "carga_horaria": 60
+    },
+    {
+        "codigo_turma": "T02",
+        "disciplina": "Banco de Dados",
+        "carga_horaria": 60
+    },
+    {
+        "codigo_turma": "T03",
+        "disciplina": "Matematica",
+        "carga_horaria": 80
+    },
+    {
+        "codigo_turma": "T04",
+        "disciplina": "Portugues",
+        "carga_horaria": 60
+    },
+    {
+        "codigo_turma": "T05",
+        "disciplina": "Ingles",
+        "carga_horaria": 40
+    },
+    {
+        "codigo_turma": "T06",
+        "disciplina": "Algoritmos",
+        "carga_horaria": 60
+    },
+    {
+        "codigo_turma": "T07",
+        "disciplina": "Redes",
+        "carga_horaria": 60
+    },
+    {
+        "codigo_turma": "T08",
+        "disciplina": "Sistemas Operacionais",
+        "carga_horaria": 60
+    },
+    {
+        "codigo_turma": "T09",
+        "disciplina": "Engenharia de Software",
+        "carga_horaria": 60
+    },
+    {
+        "codigo_turma": "T10",
+        "disciplina": "Projeto de Sistemas",
+        "carga_horaria": 80
+    }
+]
+print(turmas)
