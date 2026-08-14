ebbef7ec429cc401cdc227100c401a7c003a6247
Paulo Tácito <106617036+PauloTacito@users.noreply.github.com>
2026-08-14 10:10:28 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,14 @@
+print ("=" * 70)
+print ("            DIÁRIO DE CLASSE ELETRÔNICO - MÓDULO DOCENTE")
+print ("=" * 70)
+
+print ("    [A] Cadastrar Alunos e Turmas")
+print ("    [B] Iniciar Chamada Diária (Lancamento por Aluno)")
+print ("    [C] Calcular Frequência e  Emitir Alerta de Reprovação")
+print ("    [D] Processar Abono de Faltas (Atestados)")
+print ("    [E] Sincronizar Arquivo em Disco (presencas.txt / pickle)")
+print ("    [F] Sair")
+
+print ("-" * 70)
+
+input ("Opção Digitada:")
